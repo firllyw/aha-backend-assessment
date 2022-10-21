14de5ccf626a00e3333e1c555146ca6b79072b58
firllyw <fwahyudi17@gmail.com>
2022-10-21 13:48:01 +0700

    new origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://178.79.159.75:2222/'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Aha Assesment Backend Engineer')
