eec4e2759451bb3db24791d5e149402565f33bf1
firllyw <fwahyudi17@gmail.com>
2022-10-21 14:10:55 +0700

    global .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionFilter } from './common/filter/app.excp.filter';
 import { GoogleStrategy } from './google.strategy';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
   providers: [
     GoogleStrategy,
